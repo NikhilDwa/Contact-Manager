ec83c5a4414146a825bdeb454bef2914471f3aac
Nikhil <nikhildwa627@gmail.com>
2022-01-16 16:28:48 +0545

    Example for config file

---
+++ src/config/colors_example.js
@@ -0,0 +1,10 @@
+export const Colors = {
+    primaryBackgroundColor: "#f3f3f3",
+    primary: "#1e4fce",
+    secondary: "#e2641d",
+    inputFieldColor: "#fafafa",
+    mutedTextColor: "#8c8c8c",
+    white: "#fff",
+    black: "#000",
+    red: "#ff3232",
+};
