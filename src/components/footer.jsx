1959ca0bb051ba0602cb581249b642ff62dc834d
Nikhil <nikhildwa627@gmail.com>
2022-01-16 16:32:16 +0545

    Addition of Footer for Contact Manager

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer fixed-bottom py-3 bg-white ">
+            <div className="container">
+                <p className="text-muted text-center">
+                    Copyright 2022. Contact Manager
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
