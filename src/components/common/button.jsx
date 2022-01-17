2954c2c50aa5b7e62693cc1133a4b875b0cbab60
Nikhil <nikhildwa627@gmail.com>
2022-01-17 15:37:25 +0545

    Created reusable button

---
+++ src/components/common/button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Button = ({
+    className,
+    borderColor = "",
+    backgroundColor,
+    textColor = "#000",
+    text,
+}) => {
+    return (
+        <button
+            style={{
+                borderRadius: "3.7vw",
+                fontWeight: "bold",
+                backgroundColor: backgroundColor,
+                color: textColor,
+                border: borderColor,
+                whiteSpace: "nowrap",
+                height: "100%",
+            }}
+            className={`btn btn-responsive ${className}`}
+            type="submit"
+        >
+            {text}
+        </button>
+    );
+};
+
+export default Button;
