c580f1106e404621aa199e48aa5dcbe895718a83
Nikhil <nikhildwa627@gmail.com>
2022-01-17 15:44:20 +0545

    Added addContact as props

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Colors } from "../config/colors";
-
-const NavBar = () => {
+import Button from "../components/common/button";
+const NavBar = ({ addContact }) => {
     return (
         <nav className="navbar navbar-light bg-white justify-content-between px-5">
             <Link className="navbar-brand" to="/">
@@ -16,6 +16,19 @@ const NavBar = () => {
                 >
                     Contact Manager
                 </div>
+                {addContact && (
+                    <Link
+                        style={{
+                            height: "100%",
+                        }}
+                        to="/addContactForm"
+                    >
+                        <Button
+                            text="Add Contact"
+                            borderColor="1px solid black"
+                        />
+                    </Link>
+                )}
             </Link>
         </nav>
     );
